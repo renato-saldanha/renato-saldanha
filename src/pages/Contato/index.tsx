ef1cc2c4ac7b6f4725016058c8731834a28e3add
Renato Saldanha <ranalisesaldanha@gmail.com>
2024-02-07 17:10:19 -0400

    Conclusão previa do Portifolio Criação do Contato

---
+++ src/pages/Contato/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export default function Contato() {
+  return (
+    <div className={styles.container}>
+      Contato
+    </div>
+  )
+}
--- src/pages/Portifolio/index.tsx
+++ src/pages/Portifolio/index.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image'
 import styles from './styles.module.css'
+import Img1 from '../../../public/assets/Sisflup/sis1.png'
+import Img2 from '../../../public/assets/Sisflup/sis2.png'
+import Img3 from '../../../public/assets/Sisflup/sis3.png'
+import Img4 from '../../../public/assets/Sisflup/sis4.png'
+import Img5 from '../../../public/assets/Sisflup/sis5.png'
+import Img6 from '../../../public/assets/Sisflup/sis6.png'
 
 import { Card, CardContent } from "@/components/ui/card"
 import {
@@ -11,17 +17,40 @@ import {
 } from "@/components/ui/carousel"
 
 export default function Portifolio() {
+
   return (
     <div className={styles.container}>
       <section>
         <h1>Sisflup</h1>
-        <Carousel >
-          <CarouselContent>
-            
-          </CarouselContent>
-        </Carousel>
+        <div className={styles.carrocel}>
+          <Carousel >
+            <CarouselContent>
+              {Array.from({ length: 6 }).map((_, i) => (
+                <CarouselItem key={i} className={styles.carrocelItem}>
+                  <div >
+                    <Card >
+                      <CardContent className={styles.imgContent}>
+                        <Image 
+                          alt="Img1"
+                          src={`/assets/Sisflup/sis${i + 1}.png`}
+                          quality={100}
+                          width={500}
+                          height={500}
+                        />
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
 
-        <Image alt="sisflup" src={}/>
+          
+        </div>
+
+
       </section>
       <section></section>
 
