60f6f8aafcc7b806d8fe7a34fd50fb03234aa487
Renato Saldanha <ranalisesaldanha@gmail.com>
2024-02-14 20:01:23 -0400

    implementação de contato

--- src/pages/Contato/index.tsx
+++ src/pages/Contato/index.tsx
@@ -1,9 +1,34 @@
-import styles from './styles.module.css'
+import { FormEvent, useState } from 'react'
+import Link from 'next/link'
 import { motion } from 'framer-motion'
+import { BadgeCheck } from 'lucide-react'
+
+import styles from './styles.module.css'
 
 export default function Contato() {
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [mensagem, setMensagem] = useState("")
+  const [emailEnviado, setEmailEnviado] = useState(false)
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
+function handleClick() {
+
+  setEmailEnviado(true)
+
+  setTimeout(() => {
+    setEmailEnviado(false)
+    setMensagem("")
+    setNome("")
+    setEmail("")
+  }, 15000);
+}
+
   return (
-    <motion.div
+    <motion.div className={styles.container}
       initial="hidden"
       animate="visible"
       variants={{
@@ -19,14 +44,25 @@ export default function Contato() {
           }
         }
       }}>
-      <div className={styles.container}>
-        <section>
+      <form >
+        <h2>Entre em contato comigo por email</h2>
+        <div className={styles.content}>
+          <div className={styles.containerDados}>
+            <label>Nome</label>
+            <input type='text' name={nome} value={nome} onChange={e => setNome(e.target.value)} />
+            <label>Email</label>
+            <input type='email' name={email} value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
 
-        </section>
-        <section>
-
-        </section>
-      </div>
+          <div className={styles.containerMensagem}>
+            <label>Mensagem</label>
+            <textarea name={mensagem} value={mensagem} onChange={e => setMensagem(e.target.value)} />
+          </div>
+        </div>
+        <div className={styles.containerBotao} >
+          {emailEnviado && <BadgeCheck />} <input type='button' value={emailEnviado ? "Email enviado!" : "Enviar email"} onClick={handleClick} />
+        </div>
+      </form>
     </motion.div>
   )
 }
